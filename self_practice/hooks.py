db8dad9643d85a123dffc90e9833a55ebd8438db
Alon Ben Refael <alonbr@proton.me>
2024-09-24 13:45:07 +0300

    Update hooks.py

--- self_practice/hooks.py
+++ self_practice/hooks.py
@@ -83,7 +83,7 @@ app_license = "mit"
 # ------------
 
 # before_install = "self_practice.install.before_install"
-# after_install = "self_practice.install.after_install"
+after_install = "self_practice.install.after_install"
 
 # Uninstallation
 # ------------
