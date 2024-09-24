e0195a2037c0da8db726011ce01a346f1432bac3
Alon Ben Refael <alonbr@proton.me>
2024-09-24 13:48:31 +0300

    Create install.py

---
+++ self_practice/install.py
@@ -0,0 +1,18 @@
+import frappe
+
+def after_install():
+	frappe.db.sql("""
+		INSERT
+		INTO `tabCustom HTML Block`
+		SET
+			name='Session_Calendar',
+			creation=NOW(),
+			modified=NOW(),
+			modified_by='Administrator',
+			owner='Administrator',
+			docstatus=0,
+			private=0,
+			html='<iframe src="/app/sessions-calendar/view/calendar" style="width:100%"></iframe>',
+			script='',
+			style='';
+	""")
