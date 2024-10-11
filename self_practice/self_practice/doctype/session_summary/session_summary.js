6ad40e873e9b1efe486aecb8c587abc98a347411
Alon Ben Refael <alonbr@proton.me>
2024-10-11 14:50:27 +0300

    Update session_summary.js

--- self_practice/self_practice/doctype/session_summary/session_summary.js
+++ self_practice/self_practice/doctype/session_summary/session_summary.js
@@ -1,8 +1,14 @@
 // Copyright (c) 2024, Alon Ben Refael and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Session summary", {
-// 	refresh(frm) {
+frappe.ui.form.on("Session summary", {
+  session_start(frm) {
+    frm.set_value("start",frappe.datetime.get_datetime_as_string());
+  }
+});
 
-// 	},
-// });
+frappe.ui.form.on("Session summary", {
+  session_end(frm) {
+    frm.set_value("end",frappe.datetime.get_datetime_as_string());
+  }
+});
